54d73c2082733333703757478a7b7c9eed9f4297
Guo Xiang Tan <tgx_world@hotmail.com>
2018-08-15 15:01:55 +0800

    Let's try without a thread that shuts down.

--- lib/discourse_logster_transporter/store.rb
+++ lib/discourse_logster_transporter/store.rb
@@ -11,7 +11,7 @@ module DiscourseLogsterTransporter
       @buffer = RingBuffer.new(20)
       @root_url = root_url
       @key = key
-      @thread = nil
+      start_thread
     end
 
     def report(severity, progname, message, opts = {})
@@ -34,8 +34,6 @@ module DiscourseLogsterTransporter
         progname: progname,
         opts: opts
       })
-
-      start_thread
     end
 
     private
@@ -60,16 +58,13 @@ module DiscourseLogsterTransporter
     end
 
     def start_thread
-      return if @thread&.alive? || Rails.env.test?
+      return if Rails.env.test?
 
       @thread = Thread.new do
-        last_activity = Time.zone.now.to_i
-
-        while (Time.zone.now.to_i - last_activity) < 60
+        loop do
           sleep 5
 
           if @buffer.present?
-            last_activity = Time.zone.now.to_i
             response = post
 
             if response.code.to_i == 200
