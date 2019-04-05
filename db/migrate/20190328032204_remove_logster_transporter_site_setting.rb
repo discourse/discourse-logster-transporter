b040434c282d3b8e97c8fba66b7c6c1a8cdac683
Osama Sayegh <asooomaasoooma90@gmail.com>
2019-04-05 08:49:01 +0300

    Remove `logster_transporter_ignore_regexps` site setting (#1)
    We no longer need this setting because Logster now ships with UI for ignoring logs based on regexp patterns.

---
+++ db/migrate/20190328032204_remove_logster_transporter_site_setting.rb
@@ -0,0 +1,12 @@
+class RemoveLogsterTransporterSiteSetting < ActiveRecord::Migration[5.2]
+  def up
+    execute <<~SQL
+      DELETE FROM site_settings
+      WHERE name = 'logster_transporter_ignore_regexps'
+    SQL
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
